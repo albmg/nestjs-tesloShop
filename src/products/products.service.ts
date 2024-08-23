2c3eef1b91ec20e21677730dad60ded5f6d7aa98
Alberto Merino Guerra <albmguer@gmail.com>
2024-08-23 15:33:52 +0100

    Buscar por Slug o UUID

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,10 +1,13 @@
 import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Product } from './entities/product.entity';
+
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+
+import { Product } from './entities/product.entity';
+import { validate as isUUID } from 'uuid'
 
 @Injectable()
 export class ProductsService {
@@ -41,12 +44,20 @@ export class ProductsService {
     });
   }
 
-  async findOne( id: string ) {
+  async findOne( term: string ) {
 
-    const product = await this.productRepository.findOneBy({ id })
+    let product: Product
+
+    if ( isUUID(term) ) {
+      product = await this.productRepository.findOneBy({ id: term })
+    } else {
+      product = await this.productRepository.findOneBy({ slug: term })
+    }
+
+    //const product = await this.productRepository.findOneBy({ id })
 
     if ( !product ) {
-      throw new NotFoundException(`Product with id ${ id } not found `)
+      throw new NotFoundException(`Product with ${ term } not found `)
     }
     
     return product
