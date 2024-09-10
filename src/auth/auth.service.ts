3a4f4b62f7a0f26bddeb3dfa10c384297717e68f
Alberto Merino Guerra <albmguer@gmail.com>
2024-09-10 13:26:09 +0100

    Encriptar la contraseña

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,11 @@
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+
+import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -16,11 +18,18 @@ export class AuthService {
   async create(createUserDto: CreateUserDto) {
     
     try {
-      const user = this.userRepository.create( createUserDto );
+      const { password, ...userData } = createUserDto;
+
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync( password, 10)
+      });
 
       await this.userRepository.save( user )
+      delete user.password
 
       return user;
+      // TODO: Retornar el JWT de acceso
 
 
     } catch(error) {
