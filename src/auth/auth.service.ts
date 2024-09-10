66c4ab48e52657aba9597fc7ba7071b0e7be4985
Alberto Merino Guerra <albmguer@gmail.com>
2024-09-10 14:26:47 +0100

    Login de usuario & Nest Authentication - Passport

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -6,6 +6,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 
 import * as bcrypt from 'bcrypt';
+import { LoginUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -37,6 +38,31 @@ export class AuthService {
       this.handleDBErrors(error)
     }
 
+  }
+
+  async login( loginUserDto: LoginUserDto) {
+
+    const { password, email } = loginUserDto;
+
+    const user = await this.userRepository.findOne({ 
+      where: { email },
+      select: { email: true, password: true }
+    })
+
+    if ( !user )
+      throw new UnauthorizedException('Credentials are not valid (email)')
+
+    if ( !bcrypt.compareSync( password, user.password ))
+      throw new UnauthorizedException('Credentials are not valid (password)')
+
+    return user;
+    // TODO: Retornar el JWT
+
+    try {
+
+    } catch(error) {
+      console.log(error)
+    }
   }
 
   private handleDBErrors( error: any ): never {    
