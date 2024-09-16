00c84590743e49a067fe376cce912bd0bd6ef2a2
Alberto Merino Guerra <albmguer@gmail.com>
2024-09-16 14:50:15 +0100

    Auth en otros módulos

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AuthModule } from 'src/auth/auth.module';
+
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 
@@ -8,7 +11,10 @@ import { Product, ProductImage } from './entities';
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
-  imports: [TypeOrmModule.forFeature([ Product, ProductImage ])],
+  imports: [
+    TypeOrmModule.forFeature([ Product, ProductImage ]),
+    AuthModule
+  ],
   exports: [ 
     ProductsService,
     TypeOrmModule
