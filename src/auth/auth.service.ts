0a66864fee8fd3e39d0249b2678de372c4ea7d2f
Alberto Merino Guerra <albmguer@gmail.com>
2024-09-17 14:30:42 +0100

    Check AuthStatus

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -67,13 +67,15 @@ export class AuthService {
     return {
       ...user,
       token: this.getJwtToken({ id: user.id })
-    };
-    
-    try {
+    };     
+  }
 
-    } catch(error) {
-      console.log(error)
-    }
+
+  async checkAuthStatus( user: User ) {
+    return {
+      ...user,
+      token: this.getJwtToken({ id: user.id })
+    }; 
   }
 
   
@@ -94,21 +96,5 @@ export class AuthService {
 
     throw new InternalServerErrorException('Please check server logs')
 
-  }
-
-  // findAll() {
-  //   return `This action returns all auth`;
-  // }
-
-  // findOne(id: number) {
-  //   return `This action returns a #${id} auth`;
-  // }
-
-  // update(id: number, updateAuthDto: UpdateAuthDto) {
-  //   return `This action updates a #${id} auth`;
-  // }
-
-  // remove(id: number) {
-  //   return `This action removes a #${id} auth`;
-  // }
+  }  
 }
