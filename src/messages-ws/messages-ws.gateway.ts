3c7ed022e2dced3c979f263a70818da34acc2fe0
Alberto Merino Guerra <albmguer@gmail.com>
2024-09-20 12:25:36 +0100

    Server - Mantener identificados los clientes

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -9,9 +9,14 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
 
   ) {}
   handleConnection( client: Socket ) {
-    console.log('Cliente conectado', client.id )
+    // console.log('Cliente conectado', client.id )
+    this.messagesWsService.registerClient( client )
+    console.log({ conectados: this.messagesWsService.getConnectedClients() })
   }
   handleDisconnect( client: Socket ) {
-    console.log('Cliente desconectado', client.id)        
+    // console.log('Cliente desconectado', client.id)        
+    this.messagesWsService.removeClient( client.id )
+    console.log({ conectados: this.messagesWsService.getConnectedClients() })
+
   }
 }
