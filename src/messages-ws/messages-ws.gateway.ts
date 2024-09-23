6caea653791b9525c1948de6b6869bf0b87638d3
Alberto Merino Guerra <albmguer@gmail.com>
2024-09-23 15:51:51 +0100

    Formas de emitir desde el servidor

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,6 +1,7 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
 import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from './dtos/new-message.dto';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -14,13 +15,44 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
     // console.log('Cliente conectado', client.id )
     this.messagesWsService.registerClient( client )
     // console.log({ conectados: this.messagesWsService.getConnectedClients() })
+
+    client.join('ventas');
+
+    this.wss.to('ventas').emit('');
+
     this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients())
   }
+
   handleDisconnect( client: Socket ) {
     // console.log('Cliente desconectado', client.id)        
     this.messagesWsService.removeClient( client.id )
     // console.log({ conectados: this.messagesWsService.getConnectedClients() })
     this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients())
+  }
+
+  // message-from-client
+  @SubscribeMessage('message-from-client')
+  handleMessageFromClient( client: Socket, payload: NewMessageDto ) {
+    
+    
+    // Emit only to the client!
+    // message-from-server
+    // client.emit('message-from-server', {
+    //   fullName: ' Its me!',
+    //   message: payload.message || 'no-message!!'
+    // })
+
+    // Emit everyone except initial client
+    // client.broadcast.emit('message-from-server', {
+    //   fullName: ' Its me!',
+    //   message: payload.message || 'no-message!!'
+    // })
+
+    // Emit everyone
+    this.wss.emit('message-from-server', {
+      fullName: ' Its me!',
+      message: payload.message || 'no-message!!'
+    });
 
   }
 }
